68db631fd331730add8df6e9578c2848a8d4797f
Jamie Brandon <jamie@scattered-thoughts.net>
2024-02-15 12:43:18 -0800

    Minimum viable wasm file

--- lib/Compiler.zig
+++ lib/Compiler.zig
@@ -13,6 +13,7 @@ const Self = @This();
 allocator: Allocator,
 parser: Parser,
 analyzer: Analyzer,
+wasm: ArrayList(u8),
 error_message: ?[]const u8,
 
 pub fn init(allocator: Allocator, parser: Parser, analyzer: Analyzer) Self {
@@ -20,6 +21,7 @@ pub fn init(allocator: Allocator, parser: Parser, analyzer: Analyzer) Self {
         .allocator = allocator,
         .parser = parser,
         .analyzer = analyzer,
+        .wasm = ArrayList(u8).init(allocator),
         .error_message = null,
     };
 }
@@ -29,6 +31,78 @@ pub fn compile(self: *Self) error{CompileError}!void {
 }
 
 pub fn generate(self: *Self) []u8 {
-    _ = self;
-    return &.{};
+    // Magic
+    self.emit(&.{ 0x00, 0x61, 0x73, 0x6D });
+
+    // Version
+    self.emit(&.{ 0x01, 0x00, 0x00, 0x00 });
+
+    // Function types
+    self.emit(&.{1});
+    // Bytes in section
+    self.leb_u32(4);
+    // Number of fn types.
+    self.leb_u32(1);
+    // Fn type.
+    self.emit(&.{0x60});
+    // Number of param types.
+    self.leb_u32(0);
+    // Number of result types.
+    self.leb_u32(0);
+
+    // Functions
+    self.emit(&.{3});
+    // Bytes in section
+    self.leb_u32(2);
+    // Number of fns.
+    self.leb_u32(1);
+    // Type 0.
+    self.leb_u32(0);
+
+    // Exports
+    self.emit(&.{7});
+    // Bytes in section
+    self.leb_u32(8);
+    // Number of exports.
+    self.leb_u32(1);
+    // Name
+    self.name("main");
+    // Fn export.
+    self.emit(&.{0x00});
+    // Fn 0.
+    self.leb_u32(0);
+
+    // Code
+    self.emit(&.{10});
+    // Bytes in section
+    self.leb_u32(4);
+    // Number of fns.
+    self.leb_u32(1);
+    // Bytes in fn.
+    self.leb_u32(2);
+    // Number of locals.
+    self.leb_u32(0);
+    // Body = end.
+    self.emit(&.{0x0B});
+
+    return self.wasm.items;
+}
+
+fn emit(self: *Self, bytes: []const u8) void {
+    self.wasm.appendSlice(bytes) catch oom();
+}
+
+fn leb_u32(self: *Self, i: u32) void {
+    // https://webassembly.github.io/spec/core/binary/values.html#integers
+    var n = i;
+    while (n >= (1 << 7)) {
+        self.wasm.append(@as(u8, @truncate(n)) | (1 << 7)) catch oom();
+        n = n >> 7;
+    }
+    self.wasm.append(@truncate(n)) catch oom();
+}
+
+fn name(self: *Self, string: []const u8) void {
+    self.leb_u32(@intCast(string.len));
+    self.emit(string);
 }
