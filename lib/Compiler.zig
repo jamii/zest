148ab276bc1ab2d07eda6605cb22b936648463d7
Jamie Brandon <jamie@scattered-thoughts.net>
2024-02-23 17:23:58 -0800

    Emit instructions

--- lib/Compiler.zig
+++ lib/Compiler.zig
@@ -8,14 +8,23 @@ const util = @import("./util.zig");
 const oom = util.oom;
 const Parser = @import("./Parser.zig");
 const Analyzer = @import("./Analyzer.zig");
+const ExprId = Parser.ExprId;
 
 const Self = @This();
 allocator: Allocator,
 parser: Parser,
 analyzer: Analyzer,
+scope: Scope,
 functions: ArrayList(Function),
 wasm: ArrayList(u8),
 error_message: ?[]const u8,
+
+const Scope = ArrayList(Binding);
+
+const Binding = struct {
+    name: []const u8,
+    expr_id: ExprId,
+};
 
 const Function = struct {
     name: []const u8,
@@ -32,6 +41,7 @@ pub fn init(allocator: Allocator, parser: Parser, analyzer: Analyzer) Self {
         .allocator = allocator,
         .parser = parser,
         .analyzer = analyzer,
+        .scope = Scope.init(allocator),
         .functions = ArrayList(Function).init(allocator),
         .wasm = ArrayList(u8).init(allocator),
         .error_message = null,
@@ -42,8 +52,29 @@ pub fn compile(self: *Self) error{CompileError}!void {
     self.functions.append(.{
         .name = "main",
         .locals_count = 0,
-        .body = &.{},
+        .body = try self.compileBody(self.parser.exprs.items.len - 1),
     }) catch oom();
+}
+
+fn compileBody(self: *Self, expr_id: ExprId) error{CompileError}![]Instruction {
+    var instructions = ArrayList(Instruction).init(self.allocator);
+    try self.compileExpr(expr_id, &instructions);
+    return instructions.toOwnedSlice() catch oom();
+}
+
+fn compileExpr(self: *Self, expr_id: ExprId, instructions: *ArrayList(Instruction)) error{CompileError}!void {
+    const expr = self.parser.exprs.items[expr_id];
+    switch (expr) {
+        .i64 => |num| {
+            instructions.append(.{ .i64 = num }) catch oom();
+        },
+        .statements => |statements| {
+            for (statements) |statement| {
+                try self.compileExpr(statement, instructions);
+            }
+        },
+        else => return self.fail("TODO Can't compile {}", .{expr}),
+    }
 }
 
 pub fn emitAll(self: *Self) []u8 {
@@ -110,6 +141,10 @@ pub fn emitAll(self: *Self) []u8 {
 
             self.emitLebU32(function.locals_count);
 
+            //for (function.body) |instruction| {
+            //    self.emitInstruction(instruction);
+            //}
+
             // Emit end.
             self.emitByte(0x0B);
         }
@@ -129,11 +164,29 @@ fn emitBytes(self: *Self, bs: []const u8) void {
 fn emitLebU32(self: *Self, i: u32) void {
     // https://webassembly.github.io/spec/core/binary/values.html#integers
     var n = i;
-    while (n >= (1 << 7)) {
-        self.wasm.append(@as(u8, @truncate(n)) | (1 << 7)) catch oom();
+    while (true) {
+        const chunk = @as(u8, @truncate(n & 0b0111_1111));
         n = n >> 7;
+        const encoded_chunk = if (n == 0) chunk else chunk | 0b1000_0000;
+        self.wasm.append(encoded_chunk) catch oom();
+        if (n == 0) break;
     }
-    self.wasm.append(@truncate(n)) catch oom();
+}
+
+fn emitLebU64(self: *Self, i: u64) void {
+    // https://webassembly.github.io/spec/core/binary/values.html#integers
+    var n = i;
+    while (true) {
+        const chunk = @as(u8, @truncate(n & 0b0111_1111));
+        n = n >> 7;
+        const encoded_chunk = if (n == 0) chunk else chunk | 0b1000_0000;
+        self.wasm.append(encoded_chunk) catch oom();
+        if (n == 0) break;
+    }
+}
+
+fn emitLebI64(self: *Self, i: i64) void {
+    self.emitLebU64(@bitCast(i));
 }
 
 fn emitName(self: *Self, string: []const u8) void {
@@ -182,4 +235,18 @@ fn emitSectionEnd(self: *Self, ix: usize) void {
 
 fn emitLenOf(self: *Self, slice: anytype) void {
     self.emitLebU32(@intCast(slice.len));
+}
+
+fn emitInstruction(self: *Self, instruction: Instruction) void {
+    switch (instruction) {
+        .i64 => |num| {
+            self.emitByte(0x42);
+            self.emitLebI64(num);
+        },
+    }
+}
+
+fn fail(self: *Self, comptime message: []const u8, args: anytype) error{CompileError} {
+    self.error_message = std.fmt.allocPrint(self.allocator, message, args) catch oom();
+    return error.CompileError;
 }
