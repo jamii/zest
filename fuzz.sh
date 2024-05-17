6236d21e5dfcef8c94b77724197bc499ecbbf933
Jamie Brandon <jamie@scattered-thoughts.net>
2024-05-17 16:04:03 -0700

    Move some files around

---
+++ fuzz.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -ex
+
+zig build-lib ./lib/fuzz.zig -OReleaseSafe -fPIE -fno-stack-check -lc
+hfuzz-clang libfuzz.a.o -o fuzz
+honggfuzz -i honggfuzz-corpus --linux_perf_bts_edge -P -- ./fuzz
