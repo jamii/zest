efc58712d9d1709f9bfe62acd791cd16439f2195
Jamie Brandon <jamie@scattered-thoughts.net>
2024-05-19 17:21:46 -0700

    Save smallest fuzz crash

--- fuzz-c.sh
+++ fuzz-c.sh
@@ -5,4 +5,4 @@ set -ex
 zig build-lib ./lib/fuzz.zig -OReleaseSafe -ofmt=c -lc
 zig build-obj $(zig env | jq .lib_dir -r)/compiler_rt.zig -OReleaseSafe -ofmt=c
 hfuzz-clang compiler_rt.c fuzz.c -I $(zig env | jq .lib_dir -r) -o fuzz
-honggfuzz -i honggfuzz-corpus -P -- ./fuzz
+honggfuzz -i honggfuzz-corpus -P --save_smaller -- ./fuzz
